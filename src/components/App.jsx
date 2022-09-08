72cc0c5feb2eca14a6a36401ecd9297117911bcf
Andrey Avdeev <a3avdeev@gmail.com>
2022-09-09 01:30:04 +0300

    add all folders

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,12 @@
 import user from 'data/user.json';
+import data from 'data/data.json';
+import friends from 'data/friends.json';
+import items from 'data/transactions.json';
 
-import {Profile} from 'components/Profile/Profile'
+import { Profile } from 'components/Profile/Profile'
+import { Statistics } from 'components/Statistics/Statistics'
+import { FriendsList } from "components/FriendList/FriendList"
+import {TransactionHistory} from "components/TransactionHistory/TransactionHistory"
 
 
 export const App = () => {
@@ -13,7 +19,11 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
+      <Statistics title="UPLOAD STATS" stats={data} />
+      <FriendsList friends={friends} />
+      <TransactionHistory items={items} />
     </div>
+
   );
 };
 
